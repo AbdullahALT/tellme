bdc15c09ca49925d22418ea77e05787acaa76b2a
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-10-12 14:44:23 +0300

    support different text directions

--- resources/views/partials/message.blade.php
+++ resources/views/partials/message.blade.php
@@ -1,11 +1,11 @@
 <div class="message bg-light">
 		<div class=" clearfix">			
-			<div class="message-content">
+			<div class="message-content" dir="auto">
 				{{$message->content}}
 			</div>
 			<span class="message-date text-secondary float-right">{{$message->created_at}}</span>
 		</div>
-	<div class="message-comment message-margin bg-white">
+	<div class="message-comment message-margin bg-white" dir="auto">
 		{{$message->comment}}
 	</div>
 </div>
--- resources/views/partials/unpublished-message.blade.php
+++ resources/views/partials/unpublished-message.blade.php
@@ -1,16 +1,16 @@
 <div class="message">
 	<div class="bg-light clearfix">		
-		<div class="message-content">
+		<div class="message-content" dir="auto">
 			{{$message->content}}
 		</div>
 		<span class="message-date text-secondary float-right">{{$message->created_at}}</span>
 	</div>
-		<div class="message-comment bg-light"  >
+		<div class="message-comment bg-light"  dir="auto">
 			<form action="{{route('message.comment')}}" method="post">
 				{!!csrf_field()!!}
 				<input type="hidden" name="message-id" value="{{$message->id}}">
 				<div class="write-comment" id="message-{{$message->id}}" style="display: none">
-					<textarea class="form-control" name="comment" rows="3"></textarea>
+					<textarea class="form-control" name="comment" rows="3" dir="auto"></textarea>
 				</div>
 				<button type="submit" class="btn btn-dark btn-sm button-comment" data-id="{{$message->id}}">Comment</button>
 			</form>
