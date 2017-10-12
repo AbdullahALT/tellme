22b27f12486557c7985fd871311626bc87274126
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-10-12 21:10:28 +0300

    improve the profile partial

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,10 +23,12 @@
         
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="{{route('home')}}">Tell Me</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
+            @if(Auth::check())
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            @endif
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 @if(Auth::check())
                     <ul class="navbar-nav mr-auto">
--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -1,38 +1,21 @@
-{{-- <div class="row">
-	<div class="col-md-12">
-		<div id="profile-info" class="bg-light">
+
+
+<div id="profile" class="row bg-light">
+		<div id="profile-image" class="col-md-12 col-lg-2 nopadding">
+			<img class="img-fluid" src="{{URL::to('image/users/bravely-default.jpg')}}">
+		</div>
+
+		<div id="profile-info" class="col-md-12 col-lg-10 d-flex align-content-center flex-wrap">
 			<div class="row">
-				<div id="profile-image" class="col-md-12 col-lg-2">
-					<img class="img-fluid" src="{{URL::to('image/users/bravely-default.jpg')}}">
-				</div>
-				<div id="profile-description" class="col-md-12 col-lg-10 d-flex align-items-center text-center">
-					<span> Computer science, gaming, and everything anime. Those are the ingredients you need to create a clone of me</span>
+				<div id="profile-username" class="col-md-12">
+					<h4>{{$user->name}}</h4>
 				</div>
 			</div>
-		</div>
-	</div>
-</div> --}}
-
-<div id="profile-info" class="row bg-light">
-		<div id="profile-image" class="col-md-12 col-lg-2 nopadding">
-				<img class="img-fluid" src="{{URL::to('image/users/bravely-default.jpg')}}">
-		</div>
-
-		<div id="profile-description" class="col-md-12 col-lg-10 d-flex align-items-center text-center">
-				<span> Computer science, gaming, and everything anime. Those are the ingredients you need to create a clone of me </span>
+			<div class="row">
+				<div id="profile-description" class="col-md-12">
+					<span> Computer science, gaming, and everything anime. Those are the ingredients you need to create a clone of me </span>
+				</div>
+			</div>
 		</div>
 </div>
 
-{{-- <div class="row">
-	<div id="profile-info" class="col-md-12 bg-light">
-		<div class="row">
-			<div id="profile-image" class="col-md-12 col-lg-2 nopadding">
-				<img class="img-fluid" src="{{URL::to('image/users/bravely-default.jpg')}}">
-			</div>
-
-			<div id="profile-description" class="col-md-12 col-lg-10 d-flex align-items-center text-center">
-				<span> Computer science, gaming, and everything anime. Those are the ingredients you need to create a clone of me </span>
-			</div>
-		</div>
-	</div>
-</div> --}}
