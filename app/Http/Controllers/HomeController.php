705d5ad20dbf78297edb9f0ef50fae0d9c5279fe
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-10-16 17:12:25 +0300

    Increase the amount of message shown to public

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,13 +29,8 @@ class HomeController extends Controller
     }
 
     public function profile($username){
-        //To eager load messages and order them, taken from: https://stackoverflow.com/a/18862651
-        // $user = User::with(array('messages' => function($query){
-        //     $query->orderBy('created_at','DESC');
-        // }))->where('username', $username)->first();
-        // return view('home.index')->with('user', $user);
         $user = User::where('username', $username)->first();
-        $messages = $user->messages()->where('published', '1')->orderBy('created_at', 'DESC')->paginate(5);
+        $messages = $user->messages()->where('published', '1')->orderBy('created_at', 'DESC')->paginate(10);
         return view('home.index')->with('user', $user)->with('messages', $messages);
     }
 }
