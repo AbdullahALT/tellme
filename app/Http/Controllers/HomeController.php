9b33767095e3a10e38c8a2a5604322cc9a0627ff
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-10-17 11:17:41 +0300

    change file name from index to profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,6 +31,6 @@ class HomeController extends Controller
     public function profile($username){
         $user = User::where('username', $username)->first();
         $messages = $user->messages()->where('published', '1')->orderBy('created_at', 'DESC')->paginate(10);
-        return view('home.index')->with('user', $user)->with('messages', $messages);
+        return view('home.progile')->with('user', $user)->with('messages', $messages);
     }
 }
