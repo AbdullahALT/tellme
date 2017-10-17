99d4e8af0048e7eb0945013e01c8b333b5482c29
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-10-17 17:14:19 +0300

    improve home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,5 +12,20 @@
 		<a class="btn btn-dark btn-lg d-inline-block" href="{{route('register')}}" role="btn">Register</a>
 	</p>
 </div>
+
+<div class="row on-floor">
+	<div class="col-lg-4 col-md-12 py-3">
+		<h4>Recive messages</h4>
+		<p>People may have something to tell you, they are just shy to say it in your face</p>
+	</div>
+	<div class="col-lg-4 col-md-12 py-3">
+		<h4>Response to messages</h4>
+		<p>No need for sharing what you recived on twitter, you might get unfollowed</p>
+	</div>
+	<div class="col-lg-4 col-md-12 py-3">
+		<h4>Delete messages</h4>
+		<p>Oh crap this message is hating me! I'll just delete it and nothing has happend</p>
+	</div>
+</div>
 @endsection
 
