dfe9f69c424f759232b21b82653258cce231355f
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-10-19 08:34:17 +0300

    improve private  and public user page

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -2,20 +2,21 @@
 
 <div id="profile" class="row bg-light">
 		<div id="profile-image" class="col-md-12 col-lg-2 nopadding">
-			<img class="img-fluid" src="{{URL::to('image/users/bravely-default.jpg')}}">
+			<img class="img-fluid" src="{{ URL::to('image/users/' . $user->avatar)}}">
 		</div>
 
 		<div id="profile-info" class="col-md-12 col-lg-10 d-flex align-content-center flex-wrap">
-			<div class="row">
-				<div id="profile-username" class="col-md-12">
+			{{-- Display user's name in the left if large, center otherwise --}}
+				<div class="d-none d-lg-block" dir="auto">
 					<h4>{{$user->name}}</h4>
 				</div>
-			</div>
-			<div class="row">
-				<div id="profile-description" class="col-md-12">
-					<span> Computer science, gaming, and everything anime. Those are the ingredients you need to create a clone of me </span>
+				<div class="w-100 d-lg-none d-block text-center" dir="auto">
+					<h4>{{$user->name}}</h4>
 				</div>
-			</div>
+				<div class="w-100" dir="auto">
+					<p> {{$user->bio}}</p>
+				</div>
+				
 		</div>
 </div>
 
