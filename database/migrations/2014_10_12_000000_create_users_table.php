7aaf5e20c596290b3eb8a1382c59578bb8c38516
Abdullah Al-Tamimi <abdulla20103@gmail.com>
2017-10-19 20:17:07 +0300

    adding an attribute to indicate wither the user wants his won messages to be published automatically or not

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('avatar', 20)->nullable();
             $table->string('bio', 300)->nullable();
             $table->boolean('mail_notify')->default('1');
+            $table->boolean('auto_publish')->default('0');
             $table->rememberToken();
             $table->timestamps();
         });
